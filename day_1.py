2959235f695e9d53704b984bbfcab2a9f9a825f5
@joseph3241 <joseph3241@gmail.com>
2021-12-06 09:42:29 +0000

    day 1

---
+++ day_1.py
@@ -0,0 +1,23 @@
+###############Lesson 1 - Variables
+# h = "hello"
+# w = "world"
+
+# first_number = 1299
+# second_number = 9000
+# sum = first_number + second_number
+# print(h + " " + w)
+# print("sum is:", sum)
+
+
+#############Lesson 2 - Boolean Conditions
+# print(1 == 2)
+# print(7 > 5)
+# print(7 < 5)
+
+exp_1 = 1 == 2
+exp_2 = 7 > 5
+exp_3 = 7 < 5
+
+print(exp_1 and exp_2)
+print(exp_1 or exp_2)
+print(exp_1 or exp_3)

f83b86fdc8a3511045439e2688b4c1c24b5072ec
@joseph3241 <joseph3241@gmail.com>
2022-02-07 13:52:46 +0000

    templating and urls

--- Django Polls Project/mysite/polls/views.py
+++ Django Polls Project/mysite/polls/views.py
@@ -1,10 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello World. You're at polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
 
 def details(request, question_id):
-    return HttpResponse(f"You're looking at question: {question_id}")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist!")
+    return render(request, 'polls/details.html', {'question': question})
 
 def results(request, question_id):
     return HttpResponse(f"These are the results of question {question_id}")

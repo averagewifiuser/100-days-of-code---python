8d654c6a600100d666fe2a024beb8a03d4747938
@joseph3241 <joseph3241@gmail.com>
2022-02-15 23:59:02 +0000

    db say skrr

--- Django Blog Project/myblog/blog/models.py
+++ Django Blog Project/myblog/blog/models.py
@@ -1,3 +1,24 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now) 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE) 
+
+
+    def __str__(self):
+        return f"Comment by:{self.author.username}"
--- Django Blog Project/myblog/blog/views.py
+++ Django Blog Project/myblog/blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def home(request):
     context = {
-        'title': 'Django Blog'
+        'title': 'Django Blog',
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/index.html', context)

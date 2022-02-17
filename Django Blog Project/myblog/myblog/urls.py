33457251272efbf7987b119752b7be11f1fba21b
@joseph3241 <joseph3241@gmail.com>
2022-02-17 14:54:29 +0000

    initial profilee pics

--- Django Blog Project/myblog/myblog/urls.py
+++ Django Blog Project/myblog/myblog/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as users_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -27,3 +29,6 @@ urlpatterns = [
     path('logout/', users_views.logout, name='users-logout'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

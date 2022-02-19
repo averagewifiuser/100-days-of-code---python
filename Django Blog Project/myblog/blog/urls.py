ed2db2957163bd8326c5bb85ddfc15e35c490374
@joseph3241 <joseph3241@gmail.com>
2022-02-19 01:07:31 +0000

    single user posts

--- Django Blog Project/myblog/blog/urls.py
+++ Django Blog Project/myblog/blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import (PostListView, 
+PostDetailView, PostCreateView, 
+PostUpdateView, PostDeleteView, UserPostListView)
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/<str:username>/', UserPostListView.as_view(), name='user-posts'),
     path('posts/new/', PostCreateView.as_view(), name='post-create'),
     path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),

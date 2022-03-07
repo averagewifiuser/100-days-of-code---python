97855ad2e8a35de4825e327604ce11f12848a8e4
@joseph3241 <joseph3241@gmail.com>
2022-03-07 15:35:35 +0000

    using class based views

--- Django API Project/tutorial/snippets/urls.py
+++ Django API Project/tutorial/snippets/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('snippets/', views.snippet_list, name='snippet-list'),
-    path('snippets/<int:pk>/', views.snippet_detail, name='snippet-detail'),
+    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
